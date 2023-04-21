2f62c864e9f3bbf9e6c092fea09f444854c76226
Arun kumar <106857719+ArunKumar023@users.noreply.github.com>
2023-04-21 15:31:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
+// Get the button element
+var button = document.querySelector("input[type='button']");
+
+// Get the table element
+var table = document.getElementById("myTable");
+
+// Add a click event listener to the button
+button.addEventListener("click", createTable);
+
 function createTable() {
-    //Write your code here
-  
+  // Get the number of rows from the user
+  var numRows = prompt("Input number of rows:");
+
+  // Get the number of columns from the user
+  var numCols = prompt("Input number of columns:");
+
+  // Create the table rows and cells
+  for (var i = 0; i < numRows; i++) {
+    var row = table.insertRow(i);
+    for (var j = 0; j < numCols; j++) {
+      var cell = row.insertCell(j);
+      cell.innerHTML = "Row-" + i + " Column-" + j;
+    }
+  }
 }

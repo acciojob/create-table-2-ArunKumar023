6bb62bf310351d384bea2716e21074fd34d408fa
Arun kumar <106857719+ArunKumar023@users.noreply.github.com>
2023-04-21 22:05:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,17 @@
-// Get the button element
-var button = document.querySelector("input[type='button']");
-
-// Get the table element
-var table = document.getElementById("myTable");
-
-// Add a click event listener to the button
-button.addEventListener("click", createTable);
+const createTableBtn = document.getElementById("createTableBtn");
+const myTable = document.getElementById("myTable");
 
 function createTable() {
-  // Get the number of rows from the user
-  var numRows = prompt("Input number of rows:");
+  const numRows = prompt("Input number of rows");
+  const numCols = prompt("Input number of columns");
 
-  // Get the number of columns from the user
-  var numCols = prompt("Input number of columns:");
-
-  // Create the table rows and cells
-  for (var i = 0; i < numRows; i++) {
-    var row = table.insertRow(i);
-    for (var j = 0; j < numCols; j++) {
-      var cell = row.insertCell(j);
-      cell.innerHTML = "Row-" + i + " Column-" + j;
+  for (let i = 0; i < numRows; i++) {
+    const row = myTable.insertRow(i);
+    for (let j = 0; j < numCols; j++) {
+      const cell = row.insertCell(j);
+      cell.innerHTML = `Row-${i} Column-${j}`;
     }
   }
 }
+
+createTableBtn.addEventListener("click", createTable);
